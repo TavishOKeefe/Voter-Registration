1657b3bb3e8f29e6c0320c26de9ee3b47ec8ca88
Rohan Moore and Tavish OKeefe <rohan@>
2018-10-18 08:35:39 -0700

    add check to confirm input was number

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,18 @@
-// var checkAge = function(age) {
-//   if(age >= 18) {
-//     return true;
-//   }
-//   else {
-//     return false;
-//   }
-// };
-
 $(document).ready(function() {
   $("form#age").submit(function(event) {
     event.preventDefault();
     var numAge = parseInt($("#getAge").val());
-    if(numAge >= 18) {
+    if (numAge >= 18) {
       $("#over-18").show();
       $("#age-query").hide();
-    } else {
+      $("#invalid").hide();
+    } else if (numAge < 18) {
       $("#under-18").show();
-      $("age-query").hide();
+      $("#age-query").hide();
+      $("#invalid").hide();
+    }
+    else {
+      $("#invalid").show();
     }
   });
 });
